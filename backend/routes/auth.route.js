4a1f4f7787ac9b08d6f05bbef4227d1a77b59d33
AljosaNj <aljosaa.njezic@gmail.com>
2024-09-24 16:51:26 +0200

    new update front

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { signup ,login, logout} from "../controllers/auth.controller.js"
-
+import {authCheck, signup ,login, logout} from "../controllers/auth.controller.js"
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
@@ -10,7 +10,7 @@ router.post("/login", login)
 
 router.post("/logout", logout)
 
-
+router.get("/authCheck", protectRoute, authCheck);
 
 
 export default router;
