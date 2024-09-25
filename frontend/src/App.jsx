83b6e6b895fa3719d3d1a7f504f6ae076cfa9885
AljosaNj <aljosaa.njezic@gmail.com>
2024-09-25 17:57:52 +0200

    update search

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
+import WatchPage from "./pages/WatchPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import Footer from "./components/Footer";
@@ -7,7 +8,9 @@ import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/authUser";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
-
+import SearchPage from "./pages/SearchPage";
+import SearchHistoryPage from "./pages/SearchHistoryPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   const { user, isCheckingAuth, authCheck } = useAuthStore();
@@ -34,6 +37,10 @@ function App() {
       <Route  path="/" element={<HomePage />}/>
       <Route  path="/login" element={!user ? <LoginPage /> : <Navigate to={"/"} />}/>
       <Route path='/signup' element={!user ? <SignUpPage /> : <Navigate to={"/"} />} />
+      <Route path='/watch/:id' element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
+      <Route path='/search' element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
+      <Route path='/history' element={user ? <SearchHistoryPage /> : <Navigate to={"/login"} />} />
+      <Route path='/*' element={<NotFoundPage />} />
     </Routes>
     <Footer />
 
